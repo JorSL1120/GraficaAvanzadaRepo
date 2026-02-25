c943f9f784484344d970e6029491ff49af0f5054
Jorge Salas <jorsalas1120@gmail.com>
2026-02-24 20:07:27 -0600

    agregando codigo de vectores

---
+++ Vector/Vector/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        int N = 50_000_000;
+        float[] A = new float[N];
+        float[] B = new float[N];
+        float[] C = new float[N];
+
+        Random rmd = new Random();
+
+        for(int i = 0; i < N; i++)
+        {
+            A[i] = (float)rmd.NextDouble();
+            B[i] = (float)rmd.NextDouble();
+        }
+
+        Stopwatch sw = Stopwatch.StartNew();
+
+        for(int i = 0; i < N; i++)
+        {
+            C[i] = A[i] + B[i];
+        }
+        sw.Stop();
+
+        Console.WriteLine("Tiempo CPU: " + sw.ElapsedMilliseconds + " es");
+    }
+}
