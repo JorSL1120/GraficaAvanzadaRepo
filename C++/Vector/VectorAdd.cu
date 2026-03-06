ee95211340d06197287689df1bd5f0a92237998a
Jorge Salas <jorsalas1120@gmail.com>
2026-03-05 20:52:57 -0600

    cosas de la clase

--- C++/Vector/VectorAdd.cu
+++ C++/Vector/VectorAdd.cu
@@ -1,6 +1,6 @@
 extern "C"
 __global__ void VectorAdd(float* A, float* B, float* C, int N){
-    int i = blockIdx.x * blockIdx.x + threadIdx.x;
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
 
     if(i < N)
         C[i] = A[i] + B[i];
